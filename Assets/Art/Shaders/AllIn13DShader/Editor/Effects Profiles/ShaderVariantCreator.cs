aa4d5a584f1bb4901f44b93d03c6e2edb2fd7355
Airogriller <artgun13@gmail.com>
2025-10-12 17:26:04 +0500

    Al in one 3D + spline mesher + dreamtech spline

---
+++ Assets/Art/Shaders/AllIn13DShader/Editor/Effects Profiles/ShaderVariantCreator.cs
@@ -0,0 +1,239 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace AllIn13DShader
+{
+	public static class ShaderVariantCreator
+	{
+		private const string INCLUDE_LINE_FORMAT = @"#include ""{0}""";
+		
+		private const string TAG_VARIANT_NAME = @"<VARIANT_NAME>";
+		private const string TAG_SHADER_PROPERTIES = @"<SHADER_PROPERTIES>";
+
+		private const string TAG_SHADER_PASSES_URP	= @"<SHADER_PASSES_URP>";
+		private const string TAG_SHADER_PASSES_BIRP = @"<SHADER_PASSES_BIRP>";
+
+		//
+		private const string TAG_PASS_NAME					= @"<PASS_NAME>";
+		private const string TAG_LIGHT_MODE					= @"<LIGHT_MODE>";
+		private const string TAG_BLEND_COMMAND				= @"<BLEND_COMMAND>";
+		private const string TAG_CULL_COMMAND				= @"<CULL_COMMAND>";
+		
+
+		private const string TAG_Z_WRITE_COMMAND			= @"<Z_WRITE_COMMAND>";
+
+		private const string TAG_Z_TEST_COMMAND				= @"<Z_TEST_COMMAND>";
+
+		private const string TAG_COLOR_MASK_COMMAND			= @"<COLOR_MASK_COMMAND>";
+
+
+		private const string TAG_STENCIL_BLOCK				= @"<STENCIL_BLOCK>";
+		private const string TAG_FEATURES_URP_LIBRARY		= @"<FEATURES_URP_LIBRARY>";
+		private const string TAG_VERTEX_PROGRAM_NAME		= @"<VERTEX_PROGRAM_NAME>";
+		private const string TAG_FRAGMENT_PROGRAM_NAME		= @"<FRAGMENT_PROGRAM_NAME>";
+		private const string TAG_ALL_IN_1_FEATURES			= @"<ALLIN1_FEATURES>";
+		private const string TAG_PIPELINE_PASS_SYMBOL		= @"<PIPELINE_PASS_SYMBOL>";
+		private const string TAG_THIS_PASS_SYMBOL			= @"<THIS_PASS_SYMBOL>";
+		private const string TAG_INCLUDE_PIPELINE_HELPER	= @"<INCLUDE_PIPELINE_HELPER>";
+		private const string TAG_INCLUDE_EFFECT_LIBRARIES	= @"<INCLUDE_EFFECT_LIBRARIES>";
+		private const string TAG_INCLUDE_PASS				= @"<INCLUDE_PASS>";
+		private const string TAG_EXTRA_PRAGMA_LINES			= @"<EXTRA_PRAGMA_LINES>";
+		//
+
+		private static string TEMPLATE_PATH = GlobalConfiguration.GetRootPluginFolderPath() + "/Editor/Effects Profiles/Templates/ShaderTemplate.allin1template";
+		private static string SHADER_PROPERTIES_PATH = GlobalConfiguration.GetRootPluginFolderPath() + "/Editor/Templates/AllIn13DShader_ShaderProperties.allIn13DData";
+		private static string SHADER_PASS_URP_TAMPLATE_PATH = GlobalConfiguration.GetRootPluginFolderPath() + "/Editor/Effects Profiles/Templates/PassTemplate_URP.allin1template";
+		private static string SHADER_PASS_BIRP_TAMPLATE_PATH = GlobalConfiguration.GetRootPluginFolderPath() + "/Editor/Effects Profiles/Templates/PassTemplate_BIRP.allin1template";
+
+
+		public static string SHADER_VARIANTS_FOLDER_NAME = "Baked Variants";
+		private static string SHADER_VARIANT_FILE_NAME = "AllIn13D_BakedEffects_{0}.shader";
+		
+		public static Shader CreateVariantByMaterialInfo(PropertiesConfig propertiesConfig, AbstractMaterialInfo matInfo, string profileName)
+		{
+			EffectsProfileCollection effectsProfileCollection = GlobalConfiguration.instance.effectsProfileCollection;
+			
+			EffectsProfile effectsProfile = effectsProfileCollection.CreateNewProfile(profileName);
+
+			effectsProfile.InitFromOtherProfile(effectsProfileCollection.generalProfile);
+			effectsProfileCollection.ConfigureEffectProfileByMaterialInfo(
+				target: effectsProfile, 
+				activeEffectsList: effectsProfileCollection.generalProfile, 
+				matInfo: matInfo, 
+				propertiesConfig: propertiesConfig);
+			
+			Shader res = CreateVariant(effectsProfile, effectsProfileCollection);
+			
+			return res;
+		}
+
+		public static Shader CreateVariant(EffectsProfileCollection effectsProfileCollection, string profileName)
+		{
+			EffectsProfile effectProfile = effectsProfileCollection.CreateNewProfile(profileName);
+			effectProfile.InitFromOtherProfile(effectsProfileCollection.generalProfile);
+
+			Shader res = CreateVariant(effectProfile, effectsProfileCollection);
+			return res;
+		}
+
+		public static Shader CreateVariant(EffectsProfile effectsProfile, EffectsProfileCollection effectsProfileCollection)
+		{
+			string txtShaderVariant = File.ReadAllText(TEMPLATE_PATH);
+
+			txtShaderVariant = ConfigureVariantName(txtShaderVariant, effectsProfile.profileName);
+			txtShaderVariant = ConfigureShaderProperties(txtShaderVariant);
+
+			ShaderPassCollection shaderPassCollection = GlobalConfiguration.instance.shaderPassCollection;
+
+			string urpPassesEntry = GetShaderPassesEntry(effectsProfile, shaderPassCollection, RenderPipelineEnum.URP);
+			txtShaderVariant = txtShaderVariant.Replace(TAG_SHADER_PASSES_URP, urpPassesEntry);
+
+			string birpPassesEntry = GetShaderPassesEntry(effectsProfile, shaderPassCollection, RenderPipelineEnum.BIRP);
+			txtShaderVariant = txtShaderVariant.Replace(TAG_SHADER_PASSES_BIRP, birpPassesEntry);
+
+			string folderPath = Path.Combine(Constants.SHADERS_FOLDER_PATH, SHADER_VARIANTS_FOLDER_NAME);
+			if (!AssetDatabase.IsValidFolder(folderPath))
+			{
+				AssetDatabase.CreateFolder(Constants.SHADERS_FOLDER_PATH, SHADER_VARIANTS_FOLDER_NAME);
+			}
+
+			string fileName = string.Format(SHADER_VARIANT_FILE_NAME, effectsProfile.profileName);
+			string filePath = Path.Combine(folderPath, fileName);
+			filePath = AssetDatabase.GenerateUniqueAssetPath(filePath);
+
+			effectsProfile.shaderGUID = AssetDatabase.AssetPathToGUID(filePath);
+
+			SaveFile(filePath, txtShaderVariant);
+			AssetDatabase.Refresh();
+
+			Shader res = AssetDatabase.LoadAssetAtPath(filePath, typeof(Shader)) as Shader;
+			EditorUtility.SetDirty(effectsProfileCollection);
+
+			return res;
+		}
+
+		private static void SaveFile(string filePath, string txtFile)
+		{
+			File.WriteAllText(filePath, txtFile);
+		}
+
+		private static string ConfigureVariantName(string input, string variantName)
+		{
+			string res = input.Replace(TAG_VARIANT_NAME, variantName);
+			return res;
+		}
+
+		private static string ConfigureShaderProperties(string input)
+		{
+			string res = input;
+
+			string shaderPropertiesTxt = EditorUtils.ReadFileTextWithTabs(SHADER_PROPERTIES_PATH, 2);
+			res = res.Replace(TAG_SHADER_PROPERTIES, shaderPropertiesTxt);
+
+			return res;
+		}
+
+		private static string GetShaderPassesEntry(EffectsProfile effectsProfile, ShaderPassCollection shaderPassCollection, RenderPipelineEnum renderPipeline)
+		{
+			string shaderPassTemplateTxt = EditorUtils.ReadFileTextWithTabs(GetTemplatePathByRenderPipeline(renderPipeline), 2);
+
+			List<ShaderPassConfig> shaderPasses = effectsProfile.GetShaderPasses(shaderPassCollection, renderPipeline);
+
+			bool hasStencilBlock = false;
+			for(int i = 0; i < shaderPasses.Count; i++)
+			{
+				if (shaderPasses[i].passType == AllIn13DPassType.OUTLINE)
+				{
+					hasStencilBlock = true;
+					break;
+				}
+			}
+
+			string res = string.Empty;
+			for (int i = 0; i < shaderPasses.Count; i++)
+			{
+				string currentPass = shaderPassTemplateTxt;
+				currentPass = ConfigureShaderPass(effectsProfile, currentPass, shaderPasses[i], renderPipeline, hasStencilBlock);
+
+				res += currentPass;
+				res += "\n";
+			}
+
+			return res;
+		}
+
+		private static string ConfigureShaderPass(EffectsProfile effectsProfile, string templatePass, ShaderPassConfig shaderPass, RenderPipelineEnum renderPipeline, bool hasStencilBlock)
+		{
+			string res = templatePass;
+
+			res = res.Replace(TAG_PASS_NAME, shaderPass.GetPassName(renderPipeline));
+
+			res = res.Replace(TAG_LIGHT_MODE, shaderPass.GetLightModeShaderEntry(renderPipeline));
+
+			res = res.Replace(TAG_BLEND_COMMAND, shaderPass.blendCommand.GetShaderEntry());
+			
+			res = res.Replace(TAG_CULL_COMMAND, shaderPass.cullCommand.GetShaderEntry());
+			res = res.Replace(TAG_Z_WRITE_COMMAND, shaderPass.zWriteCommand.GetShaderEntry());
+			res = res.Replace(TAG_Z_TEST_COMMAND, shaderPass.zTestCommand.GetShaderEntry());
+			res = res.Replace(TAG_COLOR_MASK_COMMAND, shaderPass.colorMaskCommand.GetShaderEntry());
+
+			if (hasStencilBlock)
+			{
+				res = res.Replace(TAG_STENCIL_BLOCK, shaderPass.GetStencilBlockShaderEntry(effectsProfile));
+			}
+			else
+			{
+				res = res.Replace(TAG_STENCIL_BLOCK, string.Empty);
+			}
+
+			res = res.Replace(TAG_FEATURES_URP_LIBRARY, shaderPass.GetPipelineFeaturesLibraryShaderEntry(renderPipeline));
+
+			res = res.Replace(TAG_VERTEX_PROGRAM_NAME, shaderPass.vertexProgramName);
+			res = res.Replace(TAG_FRAGMENT_PROGRAM_NAME, shaderPass.fragmentProgramName);
+
+			res = res.Replace(TAG_INCLUDE_PIPELINE_HELPER, string.Format(INCLUDE_LINE_FORMAT, shaderPass.GetHelperLibraryPath(renderPipeline)));
+
+			string effectsLibraries = shaderPass.GetEffectsLibrariesShaderEntry(effectsProfile);
+			res = res.Replace(TAG_INCLUDE_EFFECT_LIBRARIES, effectsLibraries);
+
+			res = res.Replace(TAG_INCLUDE_PASS, string.Format(INCLUDE_LINE_FORMAT, shaderPass.GetPassFilePath()));
+
+			List<EffectsProfileEntry> enabledEntries = effectsProfile.GetEnabledEntriesFlatList();
+			string entryFeaturesTxt = ShaderPassConfig.GetAllIn1FeaturesEntries(enabledEntries);
+			
+			res = res.Replace(TAG_ALL_IN_1_FEATURES, entryFeaturesTxt);
+
+			res = res.Replace(TAG_PIPELINE_PASS_SYMBOL, shaderPass.GetPipelinePassSymbolShaderEntry(renderPipeline));
+			res = res.Replace(TAG_THIS_PASS_SYMBOL, shaderPass.GetPassSymbolShaderEntry());
+
+			res = res.Replace(TAG_EXTRA_PRAGMA_LINES, shaderPass.GetExtraPragmaLines(renderPipeline));
+
+			return res;
+		}
+
+		private static string GetTemplatePathByRenderPipeline(RenderPipelineEnum renderPipeline)
+		{
+			string res = string.Empty;
+			switch (renderPipeline)
+			{
+				case RenderPipelineEnum.BIRP:
+					res = SHADER_PASS_BIRP_TAMPLATE_PATH;
+					break;
+
+				case RenderPipelineEnum.URP:
+					res = SHADER_PASS_URP_TAMPLATE_PATH;
+					break;
+			}
+
+			return res;
+		}
+
+		public static string GetShaderVariantsFolderPath()
+		{
+			string res = Path.Combine(Constants.SHADERS_FOLDER_PATH, SHADER_VARIANTS_FOLDER_NAME);
+			return res;
+		}
+	}
+}
